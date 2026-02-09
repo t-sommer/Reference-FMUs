0ac090e5d69e39925ab2ce43849c0ef530f63754
Torsten Sommer <torsten.sommer@3ds.com>
2026-02-09 15:40:34 +0100

    Update test

--- rust/fmusim/tests/csv_test.rs
+++ rust/fmusim/tests/csv_test.rs
@@ -1,6 +1,5 @@
-use std::{fs::{File, read_to_string}, io::Read, path::PathBuf, process::Command, str::FromStr};
+use std::{fs::read_to_string, path::PathBuf, process::Command};
 use rstest::*;
-use tempfile::TempDir;
 
 #[fixture]
 pub fn workspace_root() -> PathBuf {
@@ -9,11 +8,7 @@ pub fn workspace_root() -> PathBuf {
 }
 
 #[rstest]
-fn should_success(workspace_root: PathBuf) {
-
-    // dbg!(&workspace_root);
-
-    // let temp_dir = TempDir::new().expect("Failed to create temp directory");
+fn test_input_interpolation(workspace_root: PathBuf) {
 
     let fmu_file = workspace_root
         .join("fmusim")
@@ -23,37 +18,17 @@ fn should_success(workspace_root: PathBuf) {
         .join("2.0")
         .join("Feedthrough.fmu");
 
-    let input_file = workspace_root
-        .join("fmusim")
-        .join("tests")
-        .join("resources")
-        .join("fmi2")
-        .join("Feedthrough_interpolation_in.csv");
-
-    let output_file = workspace_root
-        .join("fmusim")
-        .join("tests")
-        .join("resources")
-        .join("fmi2")
-        .join("Feedthrough_interpolation_out.csv");
-
-    // let output_file = PathBuf::from_str(r"E:\WS\Reference-FMUs\rust\fmusim\tests\resources\fmi2\Feedthrough_types_out.csv").unwrap(); // temp_dir.path().join("Feedthrough_types_out.csv");
-
-    let expected_output_file = workspace_root
-        .join("fmusim")
-        .join("tests")
-        .join("resources")
-        .join("fmi2")
-        .join("Feedthrough_interpolation_expected.csv");
-
     let fmusim_path = workspace_root.join("target/debug/fmusim");
+    let input_file = workspace_root.join("fmusim/tests/resources/fmi2/Feedthrough_interpolation_in.csv");
+    let output_file = workspace_root.join("fmusim/tests/resources/fmi2/Feedthrough_interpolation_out.csv");
+    let expected_file = workspace_root.join("fmusim/tests/resources/fmi2/Feedthrough_interpolation_expected.csv");
 
     let simulation_output = Command::new(&fmusim_path)
         .args(&[
             &fmu_file.to_string_lossy(),
             "--input-file", &input_file.to_string_lossy(),
             "--output-file", &output_file.to_string_lossy(),
-            "--log-fmi-calls",
+            // "--log-fmi-calls",
             "--stop-time=3",
             "--output-interval=0.1",
             "--output-variable=Float64_continuous_input",
@@ -62,25 +37,13 @@ fn should_success(workspace_root: PathBuf) {
         .output()
         .expect("Failed to run fmusim");
 
-    let out = String::from_utf8_lossy(&simulation_output.stdout);
-    print!("{out}");
+    assert!(simulation_output.status.success());
 
-    let err = String::from_utf8_lossy(&simulation_output.stderr);
-    print!("{err}");
-    
-    if !simulation_output.status.success() {
-        panic!(
-            "Simulation failed: {}\nStderr: {}",
-            String::from_utf8_lossy(&simulation_output.stdout),
-            String::from_utf8_lossy(&simulation_output.stderr)
-        );
-    }
-
-    let expected = read_to_string(expected_output_file).unwrap();
+    let expected = read_to_string(expected_file).unwrap();
     let actual = read_to_string(output_file).unwrap();
     
     let expected_lines: Vec<&str> = expected.lines().collect();
     let actual_lines: Vec<&str> = actual.lines().collect();
     
-    // assert_eq!(expected_lines, actual_lines);
+    assert_eq!(expected_lines, actual_lines);
 }

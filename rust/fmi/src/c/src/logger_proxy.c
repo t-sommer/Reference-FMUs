f8ad004a965019ccb8b0c3824b1b00c61250032f
Torsten Sommer <torsten.sommer@3ds.com>
2026-04-16 14:52:27 +0200

    Allocate buffer in log_message()

--- rust/fmi/src/c/src/logger_proxy.c
+++ rust/fmi/src/c/src/logger_proxy.c
@@ -2,8 +2,6 @@
 #include <stdarg.h>
 #include "fmi2Functions.h"
 
-#define MAX_MESSAGE_LENGTH 2048
-
 #if defined _WIN32 || defined __CYGWIN__
   #define EXPORT __declspec(dllexport)
 #else
@@ -20,16 +18,32 @@ static void log_message(fmi2ComponentEnvironment componentEnvironment, fmi2Strin
     
     if (!s_logger) return;
 
-    char buffer[MAX_MESSAGE_LENGTH];
+    char* buffer = NULL;
     
     va_list args;
-    va_start(args, message);
-    
-    vsnprintf(buffer, MAX_MESSAGE_LENGTH, message, args);
 
+    va_start(args, message);
+    int len = vsnprintf(buffer, 0, message, args);
     va_end(args);
+
+    if (len < 0) return;
+
+    buffer = (char*)malloc(len + 1);
+  
+    if (!buffer) return;
+
+    va_start(args, message);
+    len = vsnprintf(buffer, len + 1, message, args);
+    va_end(args);
+
+    if (len < 0) {
+        free(buffer);
+        return;
+    }
     
     s_logger(componentEnvironment, instanceName, status, category, buffer);
+
+    free(buffer);
 }
 
 EXPORT void add_logger_proxy(fmi2CallbackFunctions *functions) {
